a775d4ccf7e5b6d88b204d7980f7620db28174f7
Dumindu <dumindukm@gmail.com>
2021-01-15 20:56:13 +0530

    created initial main project structure

---
+++ Inventory/InventoryAPI/Controllers/InventoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryAPI.Controllers
+{
+    [Route("api/[controller]/v1")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+    }
+}
